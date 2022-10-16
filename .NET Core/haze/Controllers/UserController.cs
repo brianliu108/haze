ab1be997b711e2ae48652875906cbbd828fd9c9d
shun <55934620+nikshun@users.noreply.github.com>
2022-10-16 12:44:25 -0400

    Update profile yet once more merge like 3 branches because some people wrote their code on top of my code and then blame me for not doing anyhting even though they don't even know what I have been doing for literally hours and hours on end my life sucks sometimes I think of just ending it all but for some reason I don't have enough guts to take needed actiosns so I hope someone can help me and just finish what I can;'t

--- .NET Core/haze/Controllers/UserController.cs
+++ .NET Core/haze/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using haze.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Linq;
 
 namespace haze.Controllers
 {
@@ -19,16 +20,16 @@ namespace haze.Controllers
         }
 
         [HttpGet("GetUsers")]
-        [Authorize(Roles ="Admin")]
+        [Authorize(Roles = "Admin")]
         public async Task<ActionResult<List<User>>> GetUsers()
         {
-            return Ok(await _hazeContext.Users.ToListAsync());
+            return Ok(await _hazeContext.Users.Include(x => x.FavouriteCategories).ThenInclude(x => x.Category).Include(x => x.FavouritePlatforms).ThenInclude(x => x.Platform).ToListAsync());
         }
 
         [HttpGet("/GetUser/{id}")]
-        public async Task<ActionResult<User>> GetUser(int id)
+        public async Task<ActionResult<User>> GetUser()
         {
-            var user = await _hazeContext.Users.FindAsync(id);
+            var user = await _hazeContext.Users.FirstOrDefaultAsync();
             if (user == null)
                 return BadRequest("User not found!");
             return Ok(user);
@@ -42,7 +43,116 @@ namespace haze.Controllers
             return Ok();
         }
 
+        [HttpPost("/testcreate")]
+        public async Task<IActionResult> TestCreate(User request)
+        {
+            List<FavouritePlatform> favouritePlatforms = new List<FavouritePlatform>();
+
+            Platform platform = await _hazeContext.Platforms.Where(x => x.Id == request.FavouritePlatforms[0].Platform.Id).FirstOrDefaultAsync();
+            FavouritePlatform favouritePlatform = new FavouritePlatform()
+            {
+                Platform = platform
+            };
+            favouritePlatforms.Add(favouritePlatform);
+            request.FavouritePlatforms = favouritePlatforms;
+            _hazeContext.Users.Add(request);
+            await _hazeContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPatch("/UpdateUserPreferences")]
+        [Authorize(Roles = "User")]
+        public async Task<IActionResult> UpdateUserPreferences([FromBody] UpdateUserPreferences preferences)
+        {
+            try
+            {
+                List<Category> categories = new List<Category>();
+                List<Platform> platforms = new List<Platform>();
+                List<string> errors = new List<string>();
+                if (preferences == null)
+                    return BadRequest();
+                var userId = int.Parse(HttpContext.User.Claims.Where(x => x.Type == "userId").FirstOrDefault().Value);
+                var user = await _hazeContext.Users.Include(x => x.FavouriteCategories).ThenInclude(x => x.Category).Include(x => x.FavouritePlatforms).ThenInclude(x => x.Platform).Where(x => x.Id == userId).FirstOrDefaultAsync();
+                if (preferences?.CategoryIds != null || preferences.CategoryIds?.Count > 0)
+                    categories = await _hazeContext.Categories.Where(x => preferences.CategoryIds.Contains(x.Id)).ToListAsync();
+
+                if (preferences?.PlatformIds != null || preferences?.PlatformIds?.Count > 0)
+                    platforms = await _hazeContext.Platforms.Where(x => preferences.PlatformIds.Contains(x.Id)).ToListAsync();
+
+                if (categories.Count < preferences.CategoryIds.Count)
+                    errors.Add("One or more provided Categories were not found!");
+                if (platforms.Count < preferences.PlatformIds.Count)
+                    errors.Add("One or more provided Platforms were not found!");
+                if (errors.Count > 0)
+                    return NotFound(new
+                    {
+                        Errors = errors
+                    });
+
+                if (categories.Count > 0)
+                {
+                    if (user.FavouriteCategories == null)
+                    {
+                        user.FavouriteCategories = new List<FavouriteCategory>();
+                        for (int i = 0; i < categories.Count; i++)
+                        {
+                            user.FavouriteCategories.Add(new FavouriteCategory() { Category = categories[i] });
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < categories.Count; i++)
+                        {
+                            if (user.FavouriteCategories.ElementAtOrDefault(i) != null)
+                                user.FavouriteCategories[i].Category = categories[i];
+                            else
+                                user.FavouriteCategories.Add(new FavouriteCategory() { Category = categories[i] });
+                        }
+                        int userFavCatsCount = user.FavouriteCategories.Count;
+                        for (int i = userFavCatsCount - categories.Count; i >= categories.Count; i--)
+                        {
+                            user.FavouriteCategories.RemoveAt(i);
+                        }
+                    }
+                }
+                if (platforms.Count > 0)
+                {
+                    if (user.FavouritePlatforms == null)
+                    {
+                        user.FavouritePlatforms = new List<FavouritePlatform>();
+                        for (int i = 0; i < platforms.Count; i++)
+                        {
+                            user.FavouritePlatforms.Add(new FavouritePlatform() { Platform = platforms[i] });
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < categories.Count; i++)
+                        {
+                            if (user.FavouritePlatforms.ElementAtOrDefault(i) != null)
+                                user.FavouritePlatforms[i].Platform = platforms[i];
+                            else
+                                user.FavouritePlatforms.Add(new FavouritePlatform() { Platform = platforms[i] });
+                        }
+                        int userFavPlatsCount = user.FavouritePlatforms.Count;
+                        for (int i = userFavPlatsCount - platforms.Count; i >= platforms.Count; i--)
+                        {
+                            user.FavouritePlatforms.RemoveAt(i);
+                        }
+                    }
+                }
+                await _hazeContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+
         [HttpPut("/UpdateUser")]
+        [Authorize(Roles = "User")]
         public async Task<IActionResult> ProfileUpdate(User request)
         {
             User user = await _hazeContext.Users.FindAsync(request.Id);
@@ -51,7 +161,11 @@ namespace haze.Controllers
 
             var test = HttpContext.User;
 
-            if (test.Identity.IsAuthenticated)
+            if (test == null || !test.Identity.IsAuthenticated)
+            {
+                return BadRequest("User not Authenticated!");
+            }
+            else
             {
                 user.BirthDate = request.BirthDate;
                 user.Username = request.Username;
@@ -62,15 +176,12 @@ namespace haze.Controllers
                 user.Gender = request.Gender;
                 user.Newsletter = request.Newsletter;
                 user.RoleName = request.RoleName;
-
-                await _hazeContext.SaveChangesAsync();
-
-                return Ok();
             }
-            else
-            {
-                return BadRequest("User not Authenticated!");
-            }
+
+
+            await _hazeContext.SaveChangesAsync();
+
+            return Ok();
         }
 
         [HttpDelete("/DeleteUser/{Id}")]
@@ -80,10 +191,11 @@ namespace haze.Controllers
         }
 
         [HttpGet("/TestAuth")]
-        [Authorize(Roles ="Admin")]
+        [Authorize(Roles = "User")]
         public IActionResult TestAuthRoute()
         {
             var test = HttpContext.User;
+
             return Ok();
         }
     }
