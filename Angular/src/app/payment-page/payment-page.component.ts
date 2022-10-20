7aa8bafa0f08da0c9fd508cc44908d039f6bdbbd
brianliu108 <brianliu108@gmail.com>
2022-10-20 18:42:44 -0400

    Implement add payment info

--- Angular/src/app/payment-page/payment-page.component.ts
+++ Angular/src/app/payment-page/payment-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import axios from 'axios';
 import { AppComponent } from '../app.component';
 
 @Component({
@@ -8,19 +10,56 @@ import { AppComponent } from '../app.component';
 })
 export class PaymentPageComponent implements OnInit {
 
+  creditCardNumCtrl: FormControl = new FormControl(null, [Validators.required,
+  Validators.pattern("^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$")]);
+  expiryCtrl: FormControl = new FormControl(null, [Validators.required, Validators.pattern(`^[0-9][0-9][/][0-9][0-9]$`)]);
+  billingAddressCtrl: FormControl = new FormControl(null, Validators.required);
+  shippingAddressCtrl: FormControl = new FormControl("");
+  private token: any;
+  private requestInfo: any;
+
+  paymentGroup = new FormGroup({
+    creditCardNum: this.creditCardNumCtrl,
+    expiry: this.expiryCtrl,
+    billingAddress: this.billingAddressCtrl,
+    shippingAddress: this.shippingAddressCtrl
+  });
+
   constructor(
     private appComponent: AppComponent
   ) { }
 
   ngOnInit(): void {
+    this.token = JSON.parse(localStorage.getItem("currentUser") || '{}').token
+    this.requestInfo = {
+      headers: {
+        Authorization: "Bearer " + this.token
+      }
+    };
   }
 
-  navigateProfile(){
+  navigateProfile() {
     this.appComponent.navigate('profile');
   }
 
-  submitPaymentInfo(){
-    
+  async submitPaymentInfo() {
+    if (!this.paymentGroup.valid)
+      return;
+
+    let requestBody = {
+      "creditCardNumber": this.creditCardNumCtrl.value,
+      "expiryDate": this.expiryCtrl.value,
+      "billingAddress": this.billingAddressCtrl.value,
+      "shippingAddress": this.shippingAddressCtrl.value
+    }
+    console.log(JSON.stringify(requestBody));
+    try {
+      await axios.post("https://localhost:7105/PaymentInfo", requestBody, this.requestInfo);
+
+      this.appComponent.navigate('store');
+    } catch (error) {
+      console.log(error);
+    }
   }
 
 }
