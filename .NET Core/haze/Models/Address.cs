06a2c45783cfad456c847dad45a4501e6a1fe939
brianliu108 <brianliu108@gmail.com>
2022-11-10 13:38:13 -0500

    Add Addresses, other changes

---
+++ .NET Core/haze/Models/Address.cs
@@ -0,0 +1,12 @@
+namespace haze.Models;
+
+public class Address
+{
+    public int Id { get; set; }
+    public string StreetAddress { get; set; }
+    public string? UnitApt { get; set; }
+    public string City { get; set; }
+    public string PostalZipCode { get; set; }
+    public string Phone { get; set; }
+    public string Country { get; set; }
+}
--- .NET Core/haze/SQL Scripts/Platforms.sql
+++ .NET Core/haze/SQL Scripts/Platforms.sql
@@ -1,18 +1,3 @@
-INSERT INTO categories (name)
-  values ('shooter');
-  INSERT INTO categories (name)
-  values ('Platformer');
-  INSERT INTO categories (name)
-  values ('Rogue Like');
-  INSERT INTO categories (name)
-  values ('Strategy');
-  INSERT INTO categories (name)
-  values ('MOBA');
-  INSERT INTO categories (name)
-  values ('Role Playing Game');
-  INSERT INTO categories (name)
-  values ('Action Adventure');
-
   INSERT INTO platforms (name)
 values ('PC');
 INSERT INTO platforms (name)
