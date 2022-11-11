d7e84b5f7e7effe4775f27082114fa3b9d1be344
brianliu108 <brianliu108@gmail.com>
2022-11-11 14:26:22 -0500

    AddProvinceState

--- .NET Core/haze/Models/Address.cs
+++ .NET Core/haze/Models/Address.cs
@@ -9,4 +9,5 @@ public class Address
     public string PostalZipCode { get; set; }
     public string Phone { get; set; }
     public string Country { get; set; }
+    public string ProvinceState { get; set; }
 }
