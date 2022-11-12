d5ad56b2837226ac1255079f0b4ffe0c4f0e26cb
jamesk10118 <jamesk10118@gmail.com>
2022-11-12 14:32:41 -0500

    Implemented add/remove from wishlist

--- Angular/src/app/game-details/game-details.component.ts
+++ Angular/src/app/game-details/game-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import axios from 'axios';
+import axios, { Axios } from 'axios';
 import { AppComponent } from '../app.component';
 
 @Component({
@@ -12,8 +12,13 @@ export class GameDetailsComponent implements OnInit {
   errors: Array<any> = [];
   selectedGame: any;
   userData: any;
+  wishlistGames: Array<any>;
+
+  isInWishlist: boolean;
 
   deleted: boolean = false;
+  addedWishList: boolean = false;
+  removedWishList: boolean = false;
 
   private token: any;
   private requestInfo: any;
@@ -31,18 +36,18 @@ export class GameDetailsComponent implements OnInit {
       }
     };
 
+    this.getWishlist();
+
     this.selectedGame = JSON.parse(localStorage.getItem('selectedGame')!);
     this.userData = JSON.parse(localStorage.getItem("currentUser")!);
 
   }
 
-  routeToStore(){
+  routeToStore() {
     this.appComponent.navigate('/store');
   }
 
-  async deleteGame(){
-    console.log(this.selectedGame.id);
-
+  async deleteGame() {
     try {
       const deleteResponse = await axios.delete('https://localhost:7105/Product' + '/' + this.selectedGame.id, {
         headers: {
@@ -58,8 +63,71 @@ export class GameDetailsComponent implements OnInit {
       }
     } catch (error) {
       console.error(error);
-    }  
+    }
   }
 
-  //async addToWish
+  async addToWishlist() {
+    try {
+      let gameObj: any = {
+        "productId": this.selectedGame.id
+      };
+
+      const addWishlistResponse = await axios.post('https://localhost:7105/WishList', gameObj, this.requestInfo);
+
+      if (addWishlistResponse.status == 200) {
+        this.addedWishList = true;
+      }
+    } catch (error) {
+      this.errors.push("Game already in wishlist!");
+    }
+  }
+
+  async removeFromWishlist(){
+    try {
+      let itemId;
+      for(let item of this.wishlistGames){
+        if(item.product.id == this.selectedGame.id){
+          console.log("wishlist id:" + item.product.id);
+          console.log(this.selectedGame.id);
+          //this.isInWishlist = true;
+          itemId = item.id;
+        }
+      }
+
+      const removeWishlistResponse = await axios.delete('https://localhost:7105/WishList' + '/' + itemId, this.requestInfo);
+
+      if (removeWishlistResponse.status == 200) {
+        this.removedWishList = true;
+      }
+    } catch (error) {
+      this.errors.push("Game already removed from wishlist!");
+    }
+  }
+
+  async getWishlist(){
+    try {
+      const getWishlistResponse = await axios.get('https://localhost:7105/WishList', this.requestInfo);
+
+      if (getWishlistResponse.status == 200) {
+        this.wishlistGames = getWishlistResponse.data;
+        console.log(this.selectedGame);
+        console.log(this.wishlistGames);
+        this.checkIfInWishlist();
+      }
+    } catch (error) {
+      //this.errors.push("Game already in wishlist!");
+      console.error(error);
+    }
+  }
+
+  checkIfInWishlist(){
+    this.isInWishlist = false;
+    for(let item of this.wishlistGames){
+      if(item.product.id == this.selectedGame.id){
+        console.log("wishlist id:" + item.product.id);
+        console.log(this.selectedGame.id);
+        this.isInWishlist = true;
+      }
+    }
+  }
 }
