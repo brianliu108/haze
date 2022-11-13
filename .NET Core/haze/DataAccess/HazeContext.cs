34387cdd248bdc3852aa4488e81fac62a146dd61
shun <55934620+nikshun@users.noreply.github.com>
2022-11-13 15:54:37 -0500

    stage

--- .NET Core/haze/DataAccess/HazeContext.cs
+++ .NET Core/haze/DataAccess/HazeContext.cs
@@ -15,6 +15,7 @@ namespace haze.DataAccess
         public DbSet<Platform> Platforms { get; set; }
         public DbSet<FavouriteCategory> FavouriteCategories { get; set; }
         public DbSet<FavouritePlatform> FavouritePlatforms { get; set; }
+        public DbSet<ProductUserReview> ProductUserReviews { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductPlatform> ProductPlatforms { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
@@ -22,6 +23,5 @@ namespace haze.DataAccess
         public DbSet<EventProduct> EventProducts { get; set; }
         public DbSet<EventUser> EventUsers { get; set; }
         public DbSet<Address> Addresses { get; set; }
-        public DbSet<ProductUserReview> ProductUserReviews { get; set; }
     }
 }
