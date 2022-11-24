166d25a1465136bf8600794e94c489933f422762
brianliu108 <brianliu108@gmail.com>
2022-11-23 19:07:00 -0500

    Add 5 more tests

--- .NET Core/hazetest/HazeTest.cs
+++ .NET Core/hazetest/HazeTest.cs
@@ -56,6 +56,8 @@ public class HazeTest : IAsyncLifetime
         await RunHazeTest(endpoint, WebRequestMethods.Http.Post, true, HttpStatusCode.OK, requestBody);
     }
     
+    
+    
     // Profile Update
     [Fact]
     public async void PutProfile_ValidRequest_Ok()
@@ -74,6 +76,13 @@ public class HazeTest : IAsyncLifetime
         await RunHazeTest(endpoint, "PATCH", true, HttpStatusCode.OK, requestBody);
     }
     
+    [Fact]
+    public async void GetPreferenceSetting_ValidRequest_Ok()
+    {
+        string endpoint = "/UserPreferences";
+        await RunHazeTest(endpoint, WebRequestMethods.Http.Get, true, HttpStatusCode.OK);
+    }
+    
     // Member Address Info
     [Fact]
     public async void GetMemberAddresses_ValidRequest_Ok()
@@ -82,6 +91,14 @@ public class HazeTest : IAsyncLifetime
         await RunHazeTest(endpoint, WebRequestMethods.Http.Get, true, HttpStatusCode.OK);
     }
     
+    [Fact]
+    public async void PutMemberShippingAddress_ValidRequest_Ok()
+    {
+        string endpoint = "/ShippingAddress";
+        string requestBody =
+            "{\r\n  \"streetAddress\": \"123 Haze Rd\",\r\n  \"unitApt\": \"5401\",\r\n  \"city\": \"Toronto\",\r\n  \"postalZipCode\": \"N2M 3A3\",\r\n  \"phone\": \"123-123-1234\",\r\n  \"country\": \"Canada\",\r\n  \"provinceState\": \"ON\"\r\n}";
+        await RunHazeTest(endpoint, WebRequestMethods.Http.Put, true, HttpStatusCode.OK, requestBody);
+    }
     
     // Member Credit Card info Storing
     [Fact]
@@ -89,6 +106,15 @@ public class HazeTest : IAsyncLifetime
     {
         string endpoint = "/PaymentInfo";
         await RunHazeTest(endpoint, WebRequestMethods.Http.Get, true, HttpStatusCode.OK);
+    }
+    
+    [Fact]
+    public async void PostMemberPaymentInfo_InvalidRequest_BadRequest()
+    {
+        string endpoint = "/PaymentInfo";
+        string requestBody =
+            "{\r\n  \"creditCardNumber\": \"string\",\r\n  \"expiryDate\": \"string\"\r\n}";
+        await RunHazeTest(endpoint, WebRequestMethods.Http.Post, true, HttpStatusCode.BadRequest, requestBody);
     }
 
     // Admin Log In 
@@ -102,7 +128,24 @@ public class HazeTest : IAsyncLifetime
 
     }
     
+    [Fact]
+    public async void PostLoginAdmin_InvalidLoginCredential_NotFound()
+    {
+        string endpoint = "/Login";
+        string requestBody = $"{{\"username\": \"invalid.admin@cvgs.org\",  \"password\": \"{password}\"}}";
+
+        await RunHazeTest(endpoint, WebRequestMethods.Http.Post, true, HttpStatusCode.NotFound, requestBody, true);
+
+    }
+    
     // Admin Events Managing
+    [Fact]
+    public async void GetEvent_ValidRequest_Ok()
+    {
+        string endpoint = "/Events";
+        await RunHazeTest(endpoint, WebRequestMethods.Http.Get, true, HttpStatusCode.OK, "", true);
+    }
+    
     [Fact]
     public async void GetEvent_ValidRequest_Ok()
     {
@@ -126,6 +169,15 @@ public class HazeTest : IAsyncLifetime
         await RunHazeTest(endpoint, WebRequestMethods.Http.Get, true, HttpStatusCode.OK);
     }
     
+    /// <summary>
+    /// Runs a haze test.
+    /// </summary>
+    /// <param name="endpoint">The endpoint, such as "/Login"</param>
+    /// <param name="method">Based on WebRequestMethods.Http</param>
+    /// <param name="useToken">Whether to use the appropriate admin or member token in the request</param>
+    /// <param name="expectedStatusCode">Expected response code</param>
+    /// <param name="requestBody">The request body to use in the request</param>
+    /// <param name="isAdmin">Whether to use an admin token in the request if useToken == true</param>
     private async Task RunHazeTest(string endpoint, string method, bool useToken, HttpStatusCode expectedStatusCode, string requestBody = "", bool isAdmin = false)
     {
         try
@@ -159,6 +211,9 @@ public class HazeTest : IAsyncLifetime
                     break;
                 case "PATCH":
                     response = await client.PatchAsync(endpoint, content);
+                    break;
+                case "DELETE":
+                    response = await client.DeleteAsync(endpoint);
                     break;
             }
   
