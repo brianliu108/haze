1933a9a68988ac9c4a4052193bf05d39b26ac4a1
Brian Liu <brianliu108@gmail.com>
2022-11-28 14:17:39 -0500

    save progress desktop -> laptop

--- .NET Core/haze/Controllers/FriendController.cs
+++ .NET Core/haze/Controllers/FriendController.cs
@@ -19,7 +19,7 @@ public class FriendController : Controller
         try
         {
             var userId = int.Parse(HttpContext.User.Claims.Where(x => x.Type == "userId").FirstOrDefault().Value);
-            var friends = await GetFriendsList(true);
+            var friends = await GetFriendsList(userId, true);
             
             return Ok(friends);
         }
@@ -28,6 +28,27 @@ public class FriendController : Controller
             return BadRequest(e.Message);
         }
     }
+    
+    [HttpGet("/Friends/Requests")]
+    [Authorize]
+    public async Task<IActionResult> PendingFriendRequest(int id)
+    {
+        try
+        {
+            var userId = int.Parse(HttpContext.User.Claims.Where(x => x.Type == "userId").FirstOrDefault().Value);
+            var pendingRequests = await GetFriendsList(userId, false);
+            return Ok(new
+            {
+                PendingRequests = pendingRequests,
+                IncomingRequests = await GetIncomingRequests()
+            });
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
     [HttpPost("/Friends/Requests/Send/{id:int:required}")]
     public async Task<IActionResult> SendFriendRequest(int id)
     {
@@ -36,26 +57,21 @@ public class FriendController : Controller
             List<string> errors = new List<string>();
             var userId = int.Parse(HttpContext.User.Claims.Where(x => x.Type == "userId").FirstOrDefault().Value);
             var user = await _hazeContext.Users.Include(x => x.Friends).ThenInclude(x => x.Friend)
-                .ThenInclude(x => x.User)
+                .ThenInclude(x => x.User).Where(x => x.Id == userId)
                 .FirstOrDefaultAsync();
             var friend = await _hazeContext.Users.Where(x => x.Id == id).FirstOrDefaultAsync();
-            var currentPendingRequests = await GetFriendsList(false);
+            var currentPendingRequests = await GetFriendsList(userId, false);
             if (friend == null)
-            {
                 errors.Add("The given user wasn't found!");
+            else if (friend.Id == userId)
+                errors.Add("LMAO why u tryna add urself bro");
+            else if (currentPendingRequests.Any(a => a.ToList().Any(x => x.Friend.User.Id == friend.Id)))
+                errors.Add($"A friend request to user {friend.Username} was already sent!");
+            if (errors.Count != 0)
                 return NotFound(new
                 {
                     Errors = errors
                 });
-            }
-            if (currentPendingRequests.Any(a => a.ToList().Any(x => x.Friend.User.Id == friend.Id)))
-            {
-                errors.Add($"A friend request to user {friend.Username} was already sent!");
-                return BadRequest(new
-                {
-                    Errors = errors
-                });
-            }
             var friendsList = user.Friends.ToList();
             friendsList.Add(new UserFriend()
             {
@@ -76,21 +92,35 @@ public class FriendController : Controller
             return BadRequest(e.Message);
         }
     }
-    
-    [HttpGet("/Friends/Requests")]
+
+    [HttpPost("/Friends/Requests/Accept/{id:int:required}")]
     [Authorize]
-    public async Task<IActionResult> PendingFriendRequest(int id)
+    public async Task<IActionResult> AcceptFriend(int id)
     {
         try
         {
+            List<string> errors = new List<string>();
             var userId = int.Parse(HttpContext.User.Claims.Where(x => x.Type == "userId").FirstOrDefault().Value);
-            var pendingRequests = await GetFriendsList(false);
-            // var incomingRequests = await _hazeContext.Friends.Include(x => x.User)
-            return Ok(new
-            {
-                pendingRequests = pendingRequests
-                // incomingRequests = incomingRequests
-            });
+            var user = await _hazeContext.Users.Include(x => x.Friends).ThenInclude(x => x.Friend)
+                .ThenInclude(x => x.User).Where(x => x.Id == userId)
+                .FirstOrDefaultAsync();
+            var targetUser = await _hazeContext.Users.Include(x => x.Friends).ThenInclude(x => x.Friend)
+                .ThenInclude(x => x.User).Where(x => x.Id == id)
+                .FirstOrDefaultAsync();
+            var targetUserPendingRequests = await GetFriendsList(id, false);
+            if (targetUser == null)
+                errors.Add("The given user was not found!");
+            if (errors.Count == 0 && !targetUserPendingRequests.Any(a => a.ToList().Any(x => x.Friend.User.Id == userId)))
+                errors.Add("There isn't a pending request from the given user to accept!");
+            if (errors.Count != 0)
+                return NotFound(new
+                {
+                    Errors = errors
+                });
+            targetUser.Friends.Where(x => x.Friend.User.Id == userId).First().Friend.Accepted = true;
+
+            await _hazeContext.SaveChangesAsync();
+            return Ok();
         }
         catch (Exception e)
         {
@@ -98,17 +128,8 @@ public class FriendController : Controller
         }
     }
 
-    [HttpPost("/Friends/Requests/Accept/{id:int:required}")]
-    [Authorize]
-    public async Task<IActionResult> AcceptFriend(int id)
+    private async Task<List<IEnumerable<UserFriend>?>> GetFriendsList(int userId, bool accepted)
     {
-        
-        return Ok();
-    }
-
-    private async Task<List<IEnumerable<UserFriend>?>> GetFriendsList(bool accepted)
-    {
-        var userId = int.Parse(HttpContext.User.Claims.Where(x => x.Type == "userId").FirstOrDefault().Value);
         return await _hazeContext.Users.Include(x => x.Friends).ThenInclude(x => x.Friend)
             .ThenInclude(x => x.User).ThenInclude(x => x.FavouriteCategories).ThenInclude(x => x.Category)
             .Include(x => x.Friends).ThenInclude(x => x.Friend)
@@ -142,5 +163,46 @@ public class FriendController : Controller
                 })
             })
             .Where(x => x.Id == userId && x.Friends.Any(t => t.Friend.Accepted == accepted)).Select(x => x.Friends).ToListAsync();
+    }
+
+    private async Task<List<User>?> GetIncomingRequests()
+    {
+        var userId = int.Parse(HttpContext.User.Claims.Where(x => x.Type == "userId").FirstOrDefault().Value);
+        return await _hazeContext.Users.Include(x => x.Friends).ThenInclude(x => x.Friend)
+            .ThenInclude(x => x.User).ThenInclude(x => x.FavouriteCategories).ThenInclude(x => x.Category)
+            .Include(x => x.Friends).ThenInclude(x => x.Friend)
+            .ThenInclude(x => x.User).ThenInclude(x => x.FavouritePlatforms).ThenInclude(x => x.Platform)
+            .Include(x => x.Friends).ThenInclude(x => x.Friend)
+            .ThenInclude(x => x.User).ThenInclude(x => x.WishList)
+            .Where(x => x.Id != userId)
+            .Where(x => x.Friends.Any(a => a.Friend.User.Id == userId))
+            .Select(x => new User()
+            {
+                Id = x.Id,
+                Username = x.Username,
+                Friends = x.Friends.Select(y => new UserFriend()
+                {
+                    Id = y.Id,
+                    Friend = new Friend()
+                    {
+                        Id = y.Friend.Id,
+                        Accepted = y.Friend.Accepted,
+                        DateAccepted = y.Friend.DateAccepted,
+                        DateAdded = y.Friend.DateAdded,
+                        IsFamily = y.Friend.IsFamily,
+                        User = new User()
+                        {
+                            Id = y.Friend.User.Id,
+                            Username = y.Friend.User.Username,
+                            FirstName = y.Friend.User.FirstName,
+                            LastName = y.Friend.User.LastName,
+                            FavouriteCategories = y.Friend.User.FavouriteCategories,
+                            FavouritePlatforms = y.Friend.User.FavouritePlatforms,
+                            WishList = y.Friend.User.WishList
+                        }
+                    }
+                })
+            })
+            .ToListAsync();
     }
 }
